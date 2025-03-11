87a338084bbb9043485570dfd199ba256b0fa751
FFrelay <1rbq6yrvn@relay.firefox.com>
2025-03-11 10:37:33 +0700

    Create YGC.js

---
+++ YGC.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         YouTube Grid Customization
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @homepageURL  https://github.com/FFrelay/Userscripts
+// @updateURL    https://raw.githubusercontent.com/FFrelay/Userscripts/main/YGC.js
+// @downloadURL  https://raw.githubusercontent.com/FFrelay/Userscripts/main/YGC.js
+// @description  Modify YouTube grid layout with custom CSS
+// @author       YourName
+// @match        *://www.youtube.com/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle(`
+.style-scope.ytd-rich-grid-renderer {
+    --ytd-rich-grid-items-per-row: 4 !important;
+}
+
+#contents > ytd-rich-grid-row,
+#contents > ytd-rich-grid-row > #contents {
+    display: contents !important;
+}
+`);
