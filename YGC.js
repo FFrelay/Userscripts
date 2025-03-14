a2cdfd9254c0cb595b267aa49fd896dbd83a6021
FFrelay <8667267+FFrelay@users.noreply.github.com>
2025-03-14 09:11:44 +0700

    Update YGC.js

--- YGC.js
+++ YGC.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         YouTube Grid Customization
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @homepageURL  https://github.com/FFrelay/Userscripts
 // @updateURL    https://raw.githubusercontent.com/FFrelay/Userscripts/main/YGC.js
 // @downloadURL  https://raw.githubusercontent.com/FFrelay/Userscripts/main/YGC.js
 // @description  Modify YouTube grid layout with custom CSS
 // @author       FFrelay
 // @match        https://*.youtube.com/*
+// @exclude      https://*.youtube.com/embed/*
 // @grant        GM_addStyle
 // ==/UserScript==
 
