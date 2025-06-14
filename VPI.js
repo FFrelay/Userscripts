a9716ede24b75bd56a517a0a259e2ddeef93beb7
FFrelay <1rbq6yrvn@relay.firefox.com>
2025-06-14 10:36:51 +0700

    update

--- VPI.js
+++ VPI.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         VOZ Post Ignorer (Dark Theme) 2
+// @name         VOZ Post Ignorer (Dark Theme)
 // @namespace    https://github.com/FFrelay/Userscripts
-// @version      3.7
+// @version      3.8
 // @homepageURL  https://github.com/FFrelay/Userscripts
 // @updateURL    https://raw.githubusercontent.com/FFrelay/Userscripts/main/VPI.js
 // @downloadURL  https://raw.githubusercontent.com/FFrelay/Userscripts/main/VPI.js
